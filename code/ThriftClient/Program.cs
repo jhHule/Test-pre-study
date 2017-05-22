1af87479d5254620ab2dd20a70086db18316598d
Juha Hulkki <juha.hulkki@wapice.com>
2017-05-22 23:04:14 +0300

    t

--- code/ThriftClient/Program.cs
+++ code/ThriftClient/Program.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using storage;
+using Thrift;
+using Thrift.Protocol;
+using Thrift.Transport;
 
 namespace ThriftClient
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Apache thrift client");
+
+            try
+            {
+                TTransport transport = new TSocket("localhost", 9090);
+                TProtocol protocol = new TBinaryProtocol(transport);
+                StorageService.Client client = new StorageService.Client(protocol);
+
+                transport.Open();
+                try
+                {
+                    client.ping();
+                    Console.WriteLine("ping()");
+
+                    var list = client.storagePoints();
+                    var result = client.read(0);
+                    bool r = client.write(0, "new value");
+                    result = client.read(0);
+                
+                }
+                finally
+                {
+                    transport.Close();
+                }
+            }
+            catch (TApplicationException x)
+            {
+                Console.WriteLine(x.StackTrace);
+            }
         }
     }
 }
